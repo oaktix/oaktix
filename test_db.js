551ecefe7900ad3791db4f0e95ffc8568ad4e324
OakTix Developer <developer@oaktix.com.ng>
2026-05-30 13:48:32 +0100

    chore: remove debug test scripts

--- test_db.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-async function run() {
-  const { data: tickets } = await supabase.from('tickets').select('*').limit(1);
-  console.log(JSON.stringify(tickets, null, 2));
-}
-run();
